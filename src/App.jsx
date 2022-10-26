491a31f45281307bc7aa2aa63f6fae478d33698d
Caduaaraujool.com <caduaaraujoo@gmail.com>
2022-10-26 19:49:00 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import {Post} from "./Post";
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+
+import style from './App.module.css';
+import './global.css';
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <div className={style.wrapper}>
+    <Sidebar />
+    <main>
+      <Post
+        author="Cadu"
+        content="
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente labore at fugiat laudantium, quasi eos exercitationem vel suscipit assumenda, delectus neque mollitia similique quisquam quae non, placeat laboriosam? Fugiat, pariatur."
+      />
+         <Post
+        author="Jose"
+        content="
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente labore at fugiat laudantium, quasi eos exercitationem vel suscipit assumenda, delectus neque mollitia similique quisquam quae non, placeat laboriosam? Fugiat, pariatur."
+      />
+    </main>
+      </div>
+    </div>
+  )
+}
